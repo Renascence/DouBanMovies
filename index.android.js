1e04949ec15e79d484f7e24c5aeeed3c3f207f52
Renascence <geniusglory@outlook.com>
2016-12-15 22:51:58 +0800

    add style

--- index.android.js
+++ index.android.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, Text, Image, View ,StyleSheet} from 'react-native';
 
-class MyFirstRNProject extends Component {
+class Greeting extends Component {
   render() {
     return (
-      <Text>Hello world!</Text>
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+const style= StyleSheet.create({
+  myStyle:{
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#ccc',
+  }
+})
+
+class MyFirstRNProject extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { showText: true };
+    // 每1000毫秒对showText状态做一次取反操作
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+  render() {
+    return (
+      <View >
+        {this.state.showText ? <Text style={style.myStyle}>Hello world!</Text> : <Text style={style.myStyle}>Coder!</Text>}
+      </View>
     );
   }
 }
